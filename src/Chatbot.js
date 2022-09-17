4ad4fff602937d11a7b4eed0f8a922f7eabb1f95
Ferchx <44452156+Ferchx@users.noreply.github.com>
2022-09-16 19:53:23 -0500

    Limpieza de codigo del chatBot

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,136 +1,113 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import {w3cwebsocket} from 'websocket';
 import "./Chatbot.css"
 import { AiOutlineClose,AiOutlineSend} from "react-icons/ai";
 import React from "react";
-
-const client = new w3cwebsocket('ws://AquivaIp:AquivaPuerto');
-let primerRenderBoleano = false;
-
+//investigar hacer uso de useConext
 
 function ChatBot(){
 
-  let mensajeId;
-  let contenido;
-  let historialPasado;
-  let arrayHistorial = [];
   const claseChat = React.createRef();
   const finalChat = React.createRef();
-  let [chatAbierto,setChatAbierto] = useState(false);
+  const [chatAbierto,setChatAbierto] = useState(false);
 
 
   const cambiarEstadoChat = (estado) =>{
-    setChatAbierto(chatAbierto = !estado);
+    setChatAbierto(!estado);
     if(chatAbierto === false){
       claseChat.current.value = "";
     }
-    if(chatAbierto === true && primerRenderBoleano === false){
-      primerRender();
-    }
   };
 
-  let [mensaje,setMensaje] = useState({});
-  let [todosMensajes,setTodosMensajes] = useState([]); 
-
-  async function primeraConexion (){
-    await client.send("start");
-  }; 
+  const [todosMensajes,setTodosMensajes] = useState([]); 
+  const [client,setClient] = useState();
 
   function getHistorial(){
     return sessionStorage.getItem("Chat");
   }
 
-  const primerRender = () =>{
-    primerRenderBoleano = true;
-    historialPasado = JSON.parse(getHistorial());
-    historialPasado.forEach(element => {
-      setTodosMensajes(todosMensajes = [...todosMensajes,element]); 
-
-    });
-    historialPasado = "";
-  }
-
-  if(client.readyState === 0 && sessionStorage.getItem("Chat")){
-    client.onopen = (e) =>{
-      primeraConexion();
-    };
-    mensajeId = parseInt(sessionStorage.getItem("mensajeId"));
-  }else if(!sessionStorage.getItem('Chat') && client.readyState === 0){
-    client.onopen = (e) =>{
-      primeraConexion();
-    };
-    mensajeId = 0;
-    mensajeId = sessionStorage.setItem("mensajeId",mensajeId)
-    primerRenderBoleano = true;
-  }
-
   const  mensajeNuevoEnviado = ()=>{
-    contenido = claseChat.current.value;
-    mensajeId = parseInt(sessionStorage.getItem('mensajeId'));
-    setMensaje(mensaje = {"numeroMensaje" : mensajeId,
-          "contenidoMensaje" : {contenido},
-          "duenioMensaje" : "mensaje-persona"
-        });
+    const contenido = claseChat.current.value;
+    let mensajeId = parseInt(sessionStorage.getItem('mensajeId'));
+    const mensajePorEnviar = {"numeroMensaje" : mensajeId,
+    "contenidoMensaje" : {contenido},
+    "duenioMensaje" : "mensaje-persona"
+  }
     claseChat.current.value = "";
     mensajeId+= 1;
-    sessionStorage.setItem("mensajeId",mensajeId)
+    sessionStorage.setItem("mensajeId",mensajeId);
+    return mensajePorEnviar;
   }
+
+  const obtenerHistorialDelChat = () =>{
+    let historial = []
+      if(sessionStorage.getItem("Chat")){
+        historial = JSON.parse(getHistorial());
+      }
+    return historial;
+  };
 
   const enviarMensaje = async () =>{
     if(claseChat.current.value !== ""){
-      mensajeNuevoEnviado();
-      await client.send(mensaje.contenidoMensaje.contenido);
-      if(sessionStorage.getItem("Chat")){
-        historialPasado = JSON.parse(getHistorial());
-        historialPasado.forEach(element => {
-        arrayHistorial = arrayHistorial = ([...arrayHistorial,element]);      
-      });
-      }
-      setTodosMensajes(todosMensajes = [...todosMensajes,mensaje]);
-      arrayHistorial = arrayHistorial = ([...arrayHistorial,mensaje]);      
-      sessionStorage.setItem("Chat",JSON.stringify(arrayHistorial));
-      historialPasado = "";
-      arrayHistorial = [];
+      const mensajeFinal =  mensajeNuevoEnviado();
+      await client.send(mensajeFinal.contenidoMensaje.contenido);
+      const historial = obtenerHistorialDelChat();
+      setTodosMensajes([...historial,mensajeFinal]);
+      sessionStorage.setItem("Chat",JSON.stringify([...historial,mensajeFinal]));
     }
 
   };
 
-  const mensajeDelBot = (data) =>{
-    contenido = data;
-    mensajeId = parseInt(sessionStorage.getItem('mensajeId'));
-    if(mensajeId !== 0 && contenido === "Hola soy el asistente virtual si tienes algun problema estoy para servirte"){
-    }else{
-      setMensaje(mensaje = {"numeroMensaje" : mensajeId,
-      "contenidoMensaje" : {contenido},
-      "duenioMensaje" : "mensaje-bot"
-  });
+  const guardarMensajeDelBot = (data) =>{
+    const contenido = data;
+    let mensajeId = parseInt(sessionStorage.getItem('mensajeId'));
+    const mensajePorEnviar = {"numeroMensaje" : mensajeId,
+    "contenidoMensaje" : {contenido},
+    "duenioMensaje" : "mensaje-bot"
+};
+    
       mensajeId += 1;
       sessionStorage.setItem("mensajeId",mensajeId)
-      if(sessionStorage.getItem("Chat")){
-        historialPasado = JSON.parse(getHistorial());
-        historialPasado.forEach(element => {
-        arrayHistorial = arrayHistorial = ([...arrayHistorial,element]);      
-      });
-      }
-      setTodosMensajes(todosMensajes = [...todosMensajes,mensaje]);
-      arrayHistorial = arrayHistorial = ([...arrayHistorial,mensaje]);      
-      sessionStorage.setItem("Chat",JSON.stringify(arrayHistorial));
-      historialPasado = "";
-      arrayHistorial = [];
-    }
+      const historial = obtenerHistorialDelChat();
+      setTodosMensajes([...historial,mensajePorEnviar]);
+      sessionStorage.setItem("Chat",JSON.stringify([...historial,mensajePorEnviar]));
+    
     
   }
 
-  useEffect(() => {
-    scrollDown()
-      
-    client.onmessage  = async e => {
-      await mensajeDelBot(e.data)
-    };
-  },);
+  useEffect(() => {     
+    const wsClient = new w3cwebsocket('ws://189.163.77.95:5050');
+    setClient(wsClient);
 
-  const scrollDown = ()=>{
-    finalChat.current.scrollIntoView({ behavior : "smooth"});
+    wsClient.onmessage  = async e => {
+      await guardarMensajeDelBot(e.data)
+    };
+
+    const historialPasado = JSON.parse(getHistorial());
+    if(historialPasado){
+      setTodosMensajes(historialPasado);
+    }
+
+  if(!sessionStorage.getItem('Chat') && wsClient.readyState === 0){
+      wsClient.onopen = async () =>{
+        await wsClient.send("start");
+      };
+      
+      sessionStorage.setItem("mensajeId",0)
+    }
+
+  },[]);
+
+  useEffect(() => {
+    if(chatAbierto){
+      scrollDown()
+    }else{
+      scrollDown(true)
+    }
+  },[todosMensajes,chatAbierto])
+
+  const scrollDown = (isSmooth)=>{
+    finalChat.current.scrollIntoView(isSmooth && { behavior : "smooth"});
   };
 
   const manejarEnterChat = (e)=>{
@@ -153,8 +130,9 @@ function ChatBot(){
         </div>
         <div className="parte-inferior-chat">
           <div className="historial">
-             {todosMensajes.map((data) =>(
-                <div key={data.numeroMensaje}>
+             {todosMensajes.map((data) =>{
+              if(data?.contenidoMensaje?.contenido){
+                return <div key={data.numeroMensaje}>
                   <div className="d-flex">
                     <div className={data.duenioMensaje === "mensaje-persona" ? "mensaje-persona": "mensaje-bot"}>
                       <img className="mensaje-imagen" src={data.duenioMensaje === "mensaje-persona" ? "https://github.com/mdo.png": "https://www.ecured.cu/images/f/f5/Bot.jpg"} alt={data.duenioMensaje === "mensaje-persona" ? "imagenPerfil": "imagenBot"} />
@@ -162,7 +140,8 @@ function ChatBot(){
                     </div>
                   </div>
                 </div>
-             ))}
+              }
+              })}
              <div ref={finalChat}/>
           </div>
           <div className="espacio-escritura">
